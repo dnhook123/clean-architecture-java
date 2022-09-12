e2677e02c0b17bc40639dadeb8e46009afe265d0
dnhook123 <davidshuang3@hotmail.com>
2022-09-12 10:37:21 +0200

    Initial project setup

---
+++ src/main/java/com/todo/cleanarchitecturejava/CleanArchitectureJavaApplication.java
@@ -0,0 +1,13 @@
+package com.todo.cleanarchitecturejava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CleanArchitectureJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CleanArchitectureJavaApplication.class, args);
+	}
+
+}
